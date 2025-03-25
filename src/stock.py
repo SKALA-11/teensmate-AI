88f9915d45ee21b3f31e123c263a69a194e9e860
kimseongyu <kskksk0925@naver.com>
2025-03-25 10:06:31 +0900

    feat: Modifications for real-time stock trading system

--- src/stock.py
+++ src/stock.py
@@ -2,14 +2,21 @@ DEFAULT_INITIAL_PRICE = 0
 
 
 class Stock:
-    def __init__(self, name: str, initial_price: int = DEFAULT_INITIAL_PRICE):
+    def __init__(
+        self, name: str, tr_id: str, initial_price: int = DEFAULT_INITIAL_PRICE
+    ):
         self._name = name
+        self._id = tr_id
         self._price = initial_price
         self._price_history = [self._price]
 
     @property
     def name(self) -> str:
         return self._name
+
+    @property
+    def id(self) -> str:
+        return self._id
 
     @property
     def price(self):
@@ -20,5 +27,7 @@ class Stock:
         return self._price_history.copy()
 
     def update_price(self, new_price: int):
+        if self._price_history[0] == DEFAULT_INITIAL_PRICE:
+            self._price_history[0] = new_price
         self._price = new_price
         self._price_history.append(self._price)
