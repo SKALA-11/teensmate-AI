773205e642d787895c72d16f18c8aae74f87a8e8
kimseongyu <kskksk0925@naver.com>
2025-03-26 19:47:21 +0900

    fix: modify chroma package

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,7 +1,7 @@
 import os
 import openai
 from dotenv import load_dotenv
-from langchain_chroma import Chroma
+from langchain.vectorstores import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.schema.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.prompts import (
